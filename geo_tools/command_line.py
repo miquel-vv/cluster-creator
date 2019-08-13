13bf457e9688688ba2410b875ced31ee520ae8ba
Miquel Vande Velde/GBR <miquel.vandevelde@gmail.com>
2019-08-13 18:04:55 +0100

    Start setting up setup.py for pypi deployment.

---
+++ geo_tools/command_line.py
@@ -0,0 +1,23 @@
+import os
+from .scripts import two_step_clustering
+
+def cli():
+    import sys
+    
+    file_name = sys.argv[1]
+    if not os.path.exists(file_name): 
+        raise TypeError("{} was not found".format(file_name))
+    
+    if not os.path.isfile(file_name):
+        raise TypeError("{} is not a file".format(file_name))
+
+    return file_name
+
+def main():
+    file_name = cli()    
+    print("Starting conversion...")
+    output = create_geojson(file_name)
+    print("Geojson created at {}".format(output))
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="spatial_point_manager",
+    version="0.0.1",
+    author="Miquel Vande Velde",
+    author_email="miquel.vandevelde@gmail.com",
+    description="Contains several objects for managing lat,lng points, as well as a two step clustering algo.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/miquel-vv/cluster-creator",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'csv_to_geojson',
+        'scikit-learn',
+        'scipy',
+        'sklearn'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': ['two_step_clustering=geo_tools.scripts:main']
+    }
+)
